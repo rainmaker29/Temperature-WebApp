1eeb4f63e769f353744beca2e8f38ff9df25e497
rainmaker29 <amaanrahil29@gmail.com>
2020-11-19 12:03:59 +0530

    sol1

--- src/app/temperatureConverter/temperatureConverter.component.ts
+++ src/app/temperatureConverter/temperatureConverter.component.ts
@@ -7,10 +7,25 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 
 export class TemperatureConverter implements OnInit {
+  
+  public resfar: number;
+  public rescel: number;
 
   ngOnInit() {
     // C = (F − 32) × 5/9
     // F = C*9/5 + 32
+    this.resfar=0;
+    this.rescel=0;
+
   }
+
+  onChangeCel(event: any){
+    this.resfar = event.target.value*9/5 + 32;
+  }
+
+  onChangeFar(event: any){
+    this.rescel = (event.target.value - 32)*5/9;
+  }
+
 
 }
